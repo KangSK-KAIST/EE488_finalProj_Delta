0eb9d0180dfefeb0f13b45d8d04d4cec8d12af5f
KangSK-KAIST <54632054+KangSK-KAIST@users.noreply.github.com>
2020-07-06 23:25:45 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@
 
 cd gem5
 
-
-
 ./build/ARM/gem5.opt --outdir="../results/RWP/2MB/1" configs/finalProj/two-level.py --cpu_clock=2GHz --bench_1 --l2_size=2MB --cache_RWP --thread_max=10000000 &
 ./build/ARM/gem5.opt --outdir="../results/RWP/2MB/2" configs/finalProj/two-level.py --cpu_clock=2GHz --bench_2 --l2_size=2MB --cache_RWP --thread_max=10000000 &
 ./build/ARM/gem5.opt --outdir="../results/RWP/2MB/3" configs/finalProj/two-level.py --cpu_clock=2GHz --bench_3 --l2_size=2MB --cache_RWP --thread_max=10000000 &
